c78f11d5c04937153ff57f14660b61f24c6cdf1f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-30 09:11:48 +0100

    Creazione di test per verifica di equazione determinata

---
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EquazioniLibrary.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDetermined1()
+        {
+            double a = 3;
+
+            bool respAspettata = true;
+
+            bool resp=Equazioni.IsDetermined(a);
+
+            Assert.AreEqual(respAspettata, resp);
+
+        }
+
+        [TestMethod]
+        public void TestDetermined2()
+        {
+            double a = -5;
+
+            bool respAspettata = true;
+
+            bool resp = Equazioni.IsDetermined(a);
+
+            Assert.AreEqual(respAspettata, resp);
+
+        }
+
+        [TestMethod]
+        public void TestDetermined3()
+        {
+            double a = 0;
+
+            bool respAspettata = false;
+
+            bool resp = Equazioni.IsDetermined(a);
+
+            Assert.AreEqual(respAspettata, resp);
+
+        }
+
+
+    }
+}
---
+++ EquazioniLibrary/Equazioni.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquazioniLibrary
+{
+    public static class Equazioni
+    {
+        public static bool IsDetermined(double a)
+        {
+            bool risp = true;
+
+            if (a == 0)
+            {
+                risp = false;
+            }
+
+            return risp;
+        }
+    }
+}
