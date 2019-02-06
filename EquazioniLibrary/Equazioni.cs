3da1d184c29bbcc1f4788a01e5c87e04711f33ae
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-02-06 09:13:53 +0100

    Creazione di test per verifica di equzione impossibile

--- EquazioniLibrary/Equazioni.cs
+++ EquazioniLibrary/Equazioni.cs
@@ -19,5 +19,18 @@ namespace EquazioniLibrary
 
             return risp;
         }
+
+        public static bool IsIncosisted(double a, double b)
+        {
+            bool risp = false;
+
+            if (a == 0 && b==0)
+            {
+                risp = true;
+            }
+
+            return risp;
+        }
+
     }
 }
